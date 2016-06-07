c776fa5e6792f6c10efad7ee9fbbb5eacfbbf4c6
Craig Schwartz <craigschwartz525@gmail.com>
2016-06-07 10:40:09 -0400

    Added event view with eventId parameter

--- src/concertwishlist/urls.py
+++ src/concertwishlist/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'wishlist.views.home', name='home'),
+    url(r'^event/(?P<eventId>(\d+))/$', 'wishlist.views.event', name='event'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
